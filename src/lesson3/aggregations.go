02e464d6ff2cbbe1634ab5720372998ff6686672
Devon <miss.edwards.joseph@gmail.com>
2019-11-06 19:58:08 +0000

    Adding slicing practice

--- src/lesson3/aggregations.go
+++ src/lesson3/aggregations.go
@@ -19,5 +19,23 @@ func main() {
 	for _, v := range newArray {
 		fmt.Println("This is a person in the array: \n", v)
 	}
+
+	// Print slice of name array
+	sliceOne := newArray[0:1]
+
+	fmt.Printf("This is the slice: %v \n", sliceOne)
+
+	// Example using a slice literal, instead of slicing an existing array
+	sli := []string{"Devon", "Lizzie"}
+
+	fmt.Printf("This is the slice literal: %v \n", sli)
+
+	// Creating a slice with make()
+	mk := make([]int, 2, 3)
+	fmt.Println(mk)
+
+	// Append an item to the slice and create a new one
+	newSli := append(mk, 220)
+	fmt.Println(newSli)
 }
 
