1cd850997d6aaf7eafc3f49534c59d1d360027d9
Devon <miss.edwards.joseph@gmail.com>
2019-11-06 16:20:54 +0000

    Adding for loop example

--- src/main.go
+++ src/main.go
@@ -2,6 +2,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello world.")
+	fmt.Printf("Hello world. \n")
+
+	for i := 1; i < 10; i++ {
+		fmt.Printf("i is currently %v \n", i)
+	}
 
 }
+
