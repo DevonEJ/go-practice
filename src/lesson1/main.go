7a50bdd766112e0934e120e044eb99d480e672be
Devon <miss.edwards.joseph@gmail.com>
2019-11-06 18:29:01 +0000

    Uncommenting the first file

--- src/lesson1/main.go
+++ src/lesson1/main.go
@@ -1,20 +1,20 @@
-// package main
+package main
 
-// import "fmt"
+import "fmt"
 
-// func main() {
-// 	fmt.Printf("Hello world. \n")
+func main() {
+	fmt.Printf("Hello world. \n")
 
-// 	for i := 1; i < 10; i++ {
-// 		fmt.Printf("i is currently %v \n", i)
-// 	}
+	for i := 1; i < 10; i++ {
+		fmt.Printf("i is currently %v \n", i)
+	}
 
-// 	b := 1
-// 	for  b < 20 {
-// 		b++
-// 		if b == 5 { break }
-// 		fmt.Printf("Not yet 5. \n")
-// 	}
+	b := 1
+	for  b < 20 {
+		b++
+		if b == 5 { break }
+		fmt.Printf("Not yet 5. \n")
+	}
 
-// }
+}
 
