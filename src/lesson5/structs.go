78cd6343acb0c7e44fdc0653b544789e64d31337
Devon <miss.edwards.joseph@gmail.com>
2019-11-07 15:43:42 +0000

    Adding structs practice

---
+++ src/lesson5/structs.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	type Person struct {
+		name string
+		age  int32
+		pet  bool
+	}
+
+	p1 := new(Person)
+
+	p1.name = "Devon"
+	p1.age = 27
+	p1.pet = false
+
+	fmt.Println(p1.name)
+
+}
