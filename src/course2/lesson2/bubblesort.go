f87c3b104369d30a789140e8ee2eeaa41c1375c2
Devon <miss.edwards.joseph@gmail.com>
2019-11-08 16:03:21 +0000

    Updating bubble sort prog to take numbers as user input

--- src/course2/lesson2/bubblesort.go
+++ src/course2/lesson2/bubblesort.go
@@ -15,49 +15,59 @@ func Swap(numSlice []int, originalIndx int) {
 	numSlice[originalIndx], numSlice[swapIndx] = numSlice[swapIndx], numSlice[originalIndx]
 }
 
-//BubbleSort applies bubble sort using swapping to a slice of integers, printing the sorted slice's values
+/*
+BubbleSort takes a slice of integers, and applies the bubble sort algorithm to print the integers in ascending order.
+*/
 func BubbleSort(numbers []int) {
 
-	// Initiatlise swap slice - so we know if swaps were made on each pass through the slice or not
-	noSwaps := false
+	// Define swaps tracking variable for later use
+	var swaps bool
 
 	// Approximate a while loop
 	for {
 
-		// If noSwaps is false, continue sorting
-		if noSwaps == false {
+		// Initiatlise swap var to false as default - so we know if swaps were later made on each pass through the slice or not
+		swaps = false
 
-			// Iterate over the numbers
-			for indx, val := range numbers {
-				// If we are at the last number in the slice
-				if (indx + 1) == len(numbers) {
-					break
-				}
-				// If current value is greater than the value after it...
+		// Iterate over the numbers in the slice
+		for indx, val := range numbers {
+
+			// If we are at the last number in the slice - break - nothing to compare current value to
+			if (indx + 1) == len(numbers) {
+				break
+			} else {
+				// If current value is greater than the value after it then swap their order
 				if val > numbers[indx+1] {
 
 					Swap(numbers, indx)
-					// Set swaps tracker to true, to indicate
-					noSwaps = false
 
-				} else {
-					noSwaps = true
+					// Indicate that a swap took place
+					swaps = true
+
 				}
-
 			}
 		}
-		// If no more swaps needed to sort, print sorted slice and exit loop
-		fmt.Println(numbers)
-		break
+		// If no swaps were made in this pass of the slice, slice must be sorted, so exit loop
+		if swaps == false {
 
+			fmt.Println("Here are your sorted numbers: ", numbers)
+
+			break
+		}
 	}
 
 }
 
 func main() {
 
-	unsortedNumbers := []int{4, 1, 3, 7, 2}
+	// Define and retrieve user input - slice of integers
+	var unsortedNumbers []int
 
+	fmt.Println("Please enter up to 10 integers separate by a space: ")
+
+	fmt.Scan(&unsortedNumbers)
+
+	// Apply bubble sort
 	BubbleSort(unsortedNumbers)
 
 }
