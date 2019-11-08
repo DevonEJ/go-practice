b43ab229853a9f411099e704e10a5a03d6461876
Devon <miss.edwards.joseph@gmail.com>
2019-11-08 15:31:17 +0000

    Updating bubble sort implementation

--- src/course2/lesson2/bubblesort.go
+++ src/course2/lesson2/bubblesort.go
@@ -2,22 +2,62 @@ package main
 
 import "fmt"
 
-func Swap(pairs []int) []int {
-	return []int{0}
+/*
+Swap takes an index and a slice of integers, and swaps the order of the value at the given index, with the value
+at the next index - modifying the original slice.
+*/
+func Swap(numSlice []int, originalIndx int) {
 
+	// Define the index to move the value at originaIndx to
+	swapIndx := originalIndx + 1
+
+	// Swap the order of the desired values in the original slice
+	numSlice[originalIndx], numSlice[swapIndx] = numSlice[swapIndx], numSlice[originalIndx]
 }
 
-func BubbleSort(numbers []int) []int {
-	return []int{0}
+//BubbleSort applies bubble sort using swapping to a slice of integers, printing the sorted slice's values
+func BubbleSort(numbers []int) {
+
+	// Initiatlise swap slice - so we know if swaps were made on each pass through the slice or not
+	noSwaps := false
+
+	// Approximate a while loop
+	for {
+
+		// If noSwaps is false, continue sorting
+		if noSwaps == false {
+
+			// Iterate over the numbers
+			for indx, val := range numbers {
+				// If we are at the last number in the slice
+				if (indx + 1) == len(numbers) {
+					break
+				}
+				// If current value is greater than the value after it...
+				if val > numbers[indx+1] {
+
+					Swap(numbers, indx)
+					// Set swaps tracker to true, to indicate
+					noSwaps = false
+
+				} else {
+					noSwaps = true
+				}
+
+			}
+		}
+		// If no more swaps needed to sort, print sorted slice and exit loop
+		fmt.Println(numbers)
+		break
+
+	}
 
 }
 
 func main() {
 
-	unsortedNumbers := []int{4, 3, 1, 7, 6, 3, 1}
+	unsortedNumbers := []int{4, 1, 3, 7, 2}
 
-	res := BubbleSort(unsortedNumbers)
-
-	fmt.Println(res)
+	BubbleSort(unsortedNumbers)
 
 }
