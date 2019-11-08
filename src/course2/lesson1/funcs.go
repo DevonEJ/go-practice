601af3ed9d2c3b5a8141ffbb142d9e0645fd7b44
Devon <miss.edwards.joseph@gmail.com>
2019-11-08 12:11:04 +0000

    Adding funcs practice, setting return types

---
+++ src/course2/lesson1/funcs.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func addInts(x, y int) int {
+	res := x + y
+	return res
+}
+
+func isEven(x int) (int, bool) {
+	if x%2 == 0 {
+		return x, true
+	}
+	return x, false
+}
+
+func main() {
+
+	fmt.Println(addInts(2, 3))
+
+	fmt.Println(isEven(5))
+
+}
